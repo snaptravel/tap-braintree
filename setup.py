a548adda05ee7b35f522571f2306025a55ed8163
Ricky Rong <rrong@users.noreply.github.com>
2020-03-12 18:03:36 -0400

    upgrade singer to 5.8.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-braintree',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_braintree'],
       install_requires=[
-          'singer-python==5.5.0',
+          'singer-python==5.8.0',
           'requests==2.20.0',
           'braintree==3.53.0',
       ],
